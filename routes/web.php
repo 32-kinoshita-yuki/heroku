2cd381a0cef9f6429bc7e1e98fe79f49c51c718c
”32-kinoshita-yukigit config --global user.email ”git@github.com:32-kinoshita-yuki/sample.git <inuyasya.19950406@gmail.com>
2020-08-21 07:57:12 +0000

    second commit

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
     Route::get('news/edit', 'Admin\NewsController@edit');
     Route::post('news/edit', 'Admin\NewsController@update');
     Route::get('news/delete', 'Admin\NewsController@delete');
-    Route::get('profile', 'Admin\profileController@index');
+    Route::get('profile', 'Admin\ProfileController@index');
     Route::get('profile/create','Admin\ProfileController@add');
     Route::post('profile/create', 'Admin\ProfileController@create');
     Route::get('profile/edit','Admin\ProfileController@edit');
